0bfc7b56041b5ca13bfab2506edd125ebea17675
Nathan Moore <124376269+Mgm2023@users.noreply.github.com>
2023-06-04 22:18:06 +0100

    d2s: lineage_d2x: add support for viper fx

--- lineage_d2x.mk
+++ lineage_d2x.mk
@@ -31,6 +31,8 @@ TARGET_HAVE_SPEN := true
 ## Inherit from d2x device
 $(call inherit-product, device/samsung/d2x/device.mk)
 
+$(call inherit-product, device/samsung/exynos9820-common/v4afx/config.mk)
+
 WITH_GAPPS := 0
 # Valid for core/full GAPPS (default=true)
 TARGET_USE_GOOGLE_TELEPHONY := false
