c117e1c6c088abcc74da7ad596480a7b705b3c90
Nathan Moore <124376269+Mgm2023@users.noreply.github.com>
2023-06-06 09:53:40 +0100

    d2x: lineage_d2x: update viper fx paths

--- lineage_d2x.mk
+++ lineage_d2x.mk
@@ -31,7 +31,8 @@ TARGET_HAVE_SPEN := true
 ## Inherit from d2x device
 $(call inherit-product, device/samsung/d2x/device.mk)
 
-$(call inherit-product, device/samsung/exynos9820-common/v4afx/config.mk)
+## Inherit v4afx
+$(call inherit-product, vendor/v4afx/config.mk)
 
 WITH_GAPPS := 0
 # Valid for core/full GAPPS (default=true)
